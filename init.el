5b3c91966d184f774260dd65ed8a805852c3b3b0
Clément Dufour <clmnt.dufour@gmail.com>
2025-10-24 14:50:40 +0200

    feat: python modules

--- init.el
+++ init.el
@@ -171,8 +171,11 @@
        ;;plantuml          ; diagrams for confusing people more
        ;;graphviz          ; diagrams for confusing yourself even more
        ;;purescript        ; javascript, but functional
-       (:if (executable-find "npm")
-           (python +tree-sitter)) ; beautiful is better than ugly
+       (python
+        +lsp
+        +pyenv
+        +pyright
+        +tree-sitter)      ; beautiful is better than ugly
        ;;qt                ; the 'cutest' gui framework ever
        ;;racket            ; a DSL for DSLs
        ;;raku              ; the artist formerly known as perl6
